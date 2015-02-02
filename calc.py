df46a46a029ac656fb807e8ceead6d3bd550ba0c
jadon berghorst <jadon0827@gmail.com>
2015-02-02 09:38:32 -0600

    Master Raced calc
    Made it work on python 3

--- calc.py
+++ calc.py
@@ -1,27 +1,28 @@
 import math as m
 import time as t
-print "\nWelcome to the indirect Object Height Calculator!"
-print "---created by JP\n"
+print("\nWelcome to the indirect Object Height Calculator!")
+print("---created by JP\n")
 
 k = input("Please enter your eye-height in inches (just the number, please): ")
 theta = input("Enter the hypsometer reading: ")
-print """What measurement did you use to measure the distance from your eye to the object?
-1. inches
-2. feet
-"""
-measurement = input("> ")
-if measurement == 1:
-    x = input("Enter the distance from your eye to the object in inches: ")
-elif measurement == 2:
-    feet = input("Enter the distance from your eye to the object in feet: ")
-    x = feet * 12
-print "Calculating Trig..."
-t.sleep(1)
+print("What measurement did you use to measure the distance from your eye to the object?\n1. inches\n2. feet")
+
+while 1:
+    measurement = input("> ")
+    measurement=int(measurement)
+    if measurement == 1 or 2:
+        x = input("Enter the distance from your eye to the object in inches: ")
+        if measurement == 2:
+            x = feet * 12
+            break
+        break
+    else:
+     print("enter 1 or 2")
+print("let's go kid!\n")
 #y = x * m.tan(m.radians(theta))
 #height = y + k
-
+theta = float(theta)
+k = float(k)
+x = float(x)
 height = ( x * m.tan(m.radians(theta)) ) + k
-
-print ("The building's height is"), 
-print height,
-print "inches"
+print("The building's height is: ",height," inches")
